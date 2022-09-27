9bc199e237101ae00c6832ac4735163786fe1647
talisma <talismacassoma.manuel@uit.ac.ma>
2022-09-27 18:21:03 +0100

    bebelruntime version prblm solved

---
+++ web/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import Siriwave from "react-siriwave";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Siriwave color="#6adc92" />
+    </div>
+  );
+}
+
+//<Siriwave style="ios9" />
--- web/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return <h1>Helo World com nlw</h1>
-}
-
-export default App
