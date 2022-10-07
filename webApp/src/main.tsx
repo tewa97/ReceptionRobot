ab37872924d0b862274d6705581fc1b0a9390d9d
talisma-cassoma <talismacassoma.manuel@uit.ac.ma>
2022-10-07 16:41:12 +0000

    building section grid

--- webApp/src/main.tsx
+++ webApp/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { MapsApp } from './MapsApp'
 import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import './homeStyle.css';
  
 mapboxgl.accessToken = 'pk.eyJ1IjoiZmFjdXgxNCIsImEiOiJja3M0emhvZGExNzU0Mm9wa3pyd2tuc3c2In0.jaIs1sJoNEY_XJc1NznDCA';
 
@@ -12,6 +13,31 @@ if(!navigator.geolocation){
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <MapsApp />
+       <div className="container">
+      <aside>
+        <p>Docket</p>
+        <div className="addNote"><i className="fa fa-plus" aria-hidden="true"></i></div>
+      </aside>
+      <main>
+        <header>
+          <label htmlFor="searchLogo"><i
+              className="fa fa-search"
+              aria-hidden="true"
+            ></i></label>
+          <input
+            id="searchLogo"
+            className="searchInput"
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <h1>Notes</h1>
+        <section className="notesContainer">
+          <div className="camera"></div>
+          <div className="mapContainer"></div>
+        </section>
+      </main>
+    </div>
   </React.StrictMode>
 )
+// <MapsApp />
