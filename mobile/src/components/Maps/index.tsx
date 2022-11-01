510de8f8d40232e3ce67e46d4086f6517497122c
talisma <62837677+talisma-cassoma@users.noreply.github.com>
2022-11-01 21:00:05 +0100

    maps component

--- mobile/src/components/Maps/index.tsx
+++ mobile/src/components/Maps/index.tsx
@@ -5,8 +5,14 @@ import { styles } from './styles'
 
 export function Map(){
     return(
-        <View>
-            <MapView style = {styles.map} />
-        </View>
+      <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+       style={ styles.container }
+      />
     );
 }   
