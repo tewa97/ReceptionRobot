91f69eef45116147164918e6c0462c9fecaa5572
Talisma Manuel <talismacassoma.manuel@uit.ac.ma>
2024-02-15 01:01:01 +0100

    setup react-native-voice

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {  Pressable, StyleSheet, TextInput, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Feather} from '@expo/vector-icons' 
@@ -6,7 +6,17 @@ import { Maps } from './src/components/Maps';
 import { SearchBar } from './src/components/SearchBar';
 import { styles } from "./styles"
 
+import Voice from '@react-native-voice/voice';
+
 export default function App() {
+
+  function onSpeachResults(){
+    
+  }
+  useEffect(()=>{
+  Voice.onSpeechResults = onSpeachResults() 
+}, [])
+
   return (
     <View style={styles.container}>
       <StatusBar/>
